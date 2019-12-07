dc95215be7b0c6560de417e0d09849d73a7f9672
Spectre <EmailGax8@Gmail.com>
2019-12-08 02:06:07 +0400

    first
    first commit

---
+++ Assets/Scenes/Game/Hero/CameraMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public GameObject trackedObj;
+    public float trackSpeed;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Tracking(trackedObj);
+    }
+    public void Tracking(GameObject obj)
+    {
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, obj.transform.position.x, trackSpeed), Mathf.Lerp(transform.position.y, obj.transform.position.y, trackSpeed), transform.position.z);
+    }
+}
---
+++ Assets/Scenes/Game/Hero/HeroScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroScript : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        HeroMove();
+    }
+    public void HeroMove()
+    {
+        jump();
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
+            transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, GetComponent<Rigidbody2D>().velocity.y);
+            transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+        }
+        else
+        {
+            if(GetComponent<Rigidbody2D>().velocity.y == 0)
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+        }
+            
+
+    }
+    public void jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) & GetComponent<Rigidbody2D>().velocity.y == 0)
+        {
+            GetComponent<Rigidbody2D>().velocity += new Vector2(0, jumpForce);
+        }
+    }
+
+}
